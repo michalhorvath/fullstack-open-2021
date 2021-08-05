a8174507154678006e73cf5c8a7d42e61120dffe
Michal Horvath <michalhvt@gmail.com>
2021-08-05 21:14:04 +0200

    updates part4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -23,9 +23,6 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', middleware.tokenExtractor)
-app.use('/api/blogs', middleware.userExtractor)
-
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -9,52 +10,61 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+blogsRouter.post('/',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (request, response) => {
+    const body = request.body
 
-  const user = request.user
+    const user = request.user
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ? body.likes : 0,
-    user: user.id
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ? body.likes : 0,
+      user: user.id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
   })
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog.id)
-  await user.save()
+blogsRouter.delete('/:id',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (request, response) => {
+    const body = request.body
 
-  response.status(201).json(savedBlog)
-})
+    const user = request.user
+    const blog = await Blog.findById(request.params.id)
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const body = request.body
+    if ( blog.user.toString() !== user.id.toString() ){
+      return response.status(401).json({ error: 'no permission to delete the blog' })
+    }
 
-  const user = request.user
-  const blog = await Blog.findById(request.params.id)
+    user.blogs = user.blogs.filter(b => b.id !== blog.id)
+    await User.findByIdAndUpdate(user.id, user)
 
-  if ( blog.user.toString() !== user.id.toString() ){
-    return response.status(401).json({ error: 'no permission to delete the blog' })
-  }
+    await blog.delete()
 
-  user.blogs = user.blogs.filter(b => b.id !== blog.id)
-  await User.findByIdAndUpdate(user.id, user)
+    response.status(204).end()
+  })
 
-  await blog.delete()
-
-  response.status(204).end()
-})
-
-blogsRouter.put('/:id', async (request, response, next) => {
-  const updatedBlog =
+blogsRouter.put('/:id',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const updatedBlog =
     await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true, runValudators: true })
-  if (!updatedBlog){
-    return response.status(400).end()
-  }
-  response.json(updatedBlog)
-})
+    if (!updatedBlog){
+      return response.status(400).end()
+    }
+    response.json(updatedBlog)
+  })
 
 
 module.exports = blogsRouter
